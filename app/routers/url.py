c6f68e5cb57efabbfb07ed16320e68d2854fe853
Mark-Amgad <markamgad5@gmail.com>
2024-09-16 02:55:42 +0300

    feat(url): 1. skip url validation 2. update docs

--- app/routers/url.py
+++ app/routers/url.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+from typing import List
 from sqlalchemy.orm import Session
 from app.dependencies import get_db
 from app.schemas.url import URLCreate, URLRead
@@ -15,6 +16,7 @@ router = APIRouter(
 def create_shortened_url(url_data: URLCreate, db: Session = Depends(get_db)):
     return create_url(db=db, url_data=url_data)
 
-@router.get("/", response_model=URLRead)
+@router.get("/", response_model=List[URLRead])
 def get_shortened_urls(db: Session = Depends(get_db)):
-    return get_all_urls(db=db, limit=10,skip=0)
+    urls = get_all_urls(db=db, limit=10,skip=0)
+    return urls
